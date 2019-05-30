6282b3e24ab4cc8a25cafed08a2201b1b5cbcf19
이수인 <0330_aa@naver.com>
2019-05-30 19:12:14 +0900

    세부 정보 수정

--- ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/DetailModify.kt
+++ ppiyongppiyong4/app/src/main/java/com/cookandroid/kotlinapp/DetailModify.kt
@@ -4,14 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.header.*
-import kotlinx.android.synthetic.main.setting.*
+import kotlinx.android.synthetic.main.member_modify.*
 
 class DetailModify : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_modify)
-
-
+        btnHeaderBack.setOnClickListener {
+            val intent = Intent(this, Setting::class.java)
+            startActivity(intent)
+        }
+        btnSubmit.setOnClickListener {
+            val intent = Intent(this, Setting::class.java)
+            startActivity(intent)
+        }
     }
 }
